75ce35b73239d3cbab05d5cdde786333d189e25c
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2025-01-18 16:15:16 -0500

    wip: probes

--- app/app.py
+++ app/app.py
@@ -10,5 +10,10 @@ def helloworld():
         return jsonify(data)
 
 
+@app.route('/', methods=['GET'])
+def index():
+    return "Healthy", 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9001)
